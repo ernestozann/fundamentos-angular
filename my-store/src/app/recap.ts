a2762bca48643986d297662172b029424e5a83c6
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2022-02-09 11:04:55 -0700

    conceptos basicos de typescript para usar angular

---
+++ my-store/src/app/recap.ts
@@ -0,0 +1,13 @@
+const username: string | number = 'ernestozann';
+const sum = (a: number, b:number) => {
+  return a + b;
+}
+sum(2,5);
+
+class Person {
+  constructor(private age: number,public lastName: string) {}
+}
+
+const eru = new Person(22,"Martinez");
+eru.age;
+/*error intencional para mostrar la definicion de private */
