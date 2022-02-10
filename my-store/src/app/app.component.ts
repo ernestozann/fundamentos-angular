05468a9037a4b6359ed443fd78689c912a24c47f
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2022-02-10 11:23:14 -0700

    *ngFor para arrays

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +19,40 @@ export class AppComponent {
 
   emojis: string[] = [ '😂' , '🐦', '🐳','🌮', '💚']
   newName: string = ''
+
+  products: Product[] = [
+    {
+      name: 'El mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
 
   toogleButton() {
     this.btnDisabled = !this.btnDisabled;
---
+++ my-store/src/app/product.model.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  name: string
+  price: number
+  image: string
+  category?: string
+  /*el signo ? hace la caracteristica opcional */
+}
